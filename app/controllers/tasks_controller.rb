6a3e9bd6a02d7b2d3a1cfea44bd9b160b3b2b06e
kentaro.sugimoto <kensugi0727@gmail.com>
2020-05-11 06:24:34 +0000

    add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(25)
+    if logged_in?
+      @tasks = current_user.tasks.page(params[:page]).per(25)
+      #@tasks = Task.order(id: :desc).page(params[:page]).per(25)
+    end
   end
 
   def show
@@ -12,7 +18,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+#    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
@@ -44,7 +51,9 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'タスクは正常に削除されました'
+    #redirect_back(fallback_location: root_path)
     redirect_to tasks_url
+    
   end
 
   private
@@ -52,5 +61,12 @@ class TasksController < ApplicationController
   # Strog Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+#  root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
   root to: 'tasks#index'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  #resources :tasks, only: [:create, :destroy]
   resources :tasks
 end
